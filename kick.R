5a5b5003ae356956ea3a0a3ccb65614fa8260d58
takebayashi <take2_fmu@takebayashiyubu-no-MacBook-Air-2.local>
2018-05-21 18:03:51 +0900

    # change icon

--- kick.R
+++ kick.R
@@ -1,6 +1,6 @@
 library(blogdown)
 
 new_site(theme="Vimux/Mainroad")
-new_post(title="テスト",ext = getOption("blogdown.ext", ".rmd"))
+#new_post(title="ベイズ統計WS「心理学者のためのベイズ統計学」6/30＠専修大学（第一報）",ext = getOption("blogdown.ext", ".rmd"))
 build_site()
 serve_site()
