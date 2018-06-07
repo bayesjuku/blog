fad770c2e6fd087e8bb2f03a554c454cae5081e6
takebayashi <take2_fmu@takebayashiyubu-no-MacBook-Air-2.local>
2018-06-07 10:20:27 +0900

    # flyer modified

--- kick.R
+++ kick.R
@@ -2,7 +2,7 @@ library(blogdown)
 
 new_site(theme="Vimux/Mainroad")
 #new_post(title="ベイズ統計WS「心理学者のためのベイズ統計学」6/30＠専修大学（第一報）",ext = getOption("blogdown.ext", ".rmd"))
-new_post("2018-06-02-ws-6-30", 
+new_post("2018-06-07-ws-6-30", 
          ext = getOption("blogdown.ext", ".rmd"))
 build_site()
 
